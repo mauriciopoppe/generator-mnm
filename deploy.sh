a135c89dfdf64761bad94caeaf62b01f040de1eb
Mauricio Poppe <mauricio.poppe@gmail.com>
2015-11-26 00:03:21 -0400

    autodeploy to maurizzzio/generator-mnm-example

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# exit with non-zero exit code on failure
+set -e
+
 # Basically from https://medium.com/philosophy-logic/publishing-gh-pages-with-travis-ci-53a8270e87db
 
 cd test/.tmp
-git config user.name "Mauricio Poppe"
+git config user.name "Travis CI"
 git config user.email "mauricio.poppe@gmail.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force "https://${GH_TOKEN}@${GH_REF}" master
+git push --force "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
 
